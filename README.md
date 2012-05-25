3f343fd2288aafe3d4aae73e6a8ada2ad57ded07
Rulo Kobashikawa <akobashikawa@gmail.com>
2012-05-25 18:32:32 -0500

    support address change by scroll

--- README.md
+++ README.md
@@ -28,3 +28,4 @@ nav_fixed
 ---------
 * Páginas con anclas
 * Elementos de navegación fijos
+* Indicador de posición en el menú (sensible a url, links y scroll)
--- js/script.js
+++ js/script.js
@@ -6,19 +6,55 @@ $(document).ready(function() {
   $.address.change(function(e) {
     var id = e.value.replace(/^\//, '');
     if (id == '') {
-      id = 'page-1';// default page
+      id = 'page-0';// default page
     }
-    linkToId(id);
+    activeLinkTo(id);
   });
 
   /**
-   * Cuando se va a una página
+   * Activa el link a la página indicada
    */
-  function linkToId(id) {
+  function activeLinkTo(id) {//console.log('activeLinkTo: ' + id);
     // actualizar estilos de links del menú
     $('#menu')
-      .find('a').removeClass('active').end()
+      .find('a.active').removeClass('active').end()
       .find('a[href*="' + id + '"]').addClass('active');
   }
+
+  /**
+   * Crea un array con info de las páginas
+   */
+  var _pages = [];
+  $('.page').each(function(i, el) {
+    var top = Math.floor($(el).offset().top);
+    var id = $(el).attr('id');
+    _pages[i] = {'id': id, 'top': top};
+  });
+
+  /**
+   * Devuelve el mayor _pages[] cuyo _pages[].top
+   * sea menor al especificado.
+   */
+  function getPageForTop(top) {
+    var result = _pages[0];
+    for (i in _pages) {
+      if (_pages[i].top > top) {
+        break;
+      } else {
+        result = _pages[i];
+      }
+    }
+    return result;
+  }
+
+  /**
+   * Responde al scroll
+   */
+  var headerHeight = $('#header').height();
+  $(window).scroll(function(){
+    var top = $(window).scrollTop() + headerHeight;// compensa #header offset
+    var id = getPageForTop(top).id;//console.log('scroll: ' + id);
+    $.address.value(id);
+  });  
 
 });
